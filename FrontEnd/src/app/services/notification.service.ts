6c2c1e333f31fd117682211fac286801a7ef1c70
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-18 22:37:32 +0200

    format files and add a warning notification

--- FrontEnd/src/app/services/notification.service.ts
+++ FrontEnd/src/app/services/notification.service.ts
@@ -8,6 +8,7 @@ export class NotificationService {
 
   constructor(private toast: ToastrService) { }
 
+  // Success notification
   onSuccess(msg: string) {
     this.toast.success(msg, 'success', {
       timeOut: 2000,
@@ -15,10 +16,19 @@ export class NotificationService {
     });
   }
 
+  // Error notification
   onFail(msg: string) {
     this.toast.error(msg, 'error', {
       timeOut: 2000,
       positionClass: 'toast-bottom-right'
+    });
+  }
+
+  // Warn notification
+  onWarn(msg: string) {
+    this.toast.warning(msg, 'worn', {
+      timeOut: 2000,
+      positionClass: 'toast-bottom-right',
     });
   }
 }
