fad2bccaf335efaaeaefdf4054b5f162d578e2db
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-20 01:09:57 +0300

    edit auth.js file

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -3,7 +3,7 @@ const tokenManagement = require("../Utilis/tokenmanagement");
 const auth = async (req, res, next) => {
 
   const token = req.get("Authorization");
-  const verifiedToken = tokenManagement.verifyTokn(token);
+  const verifiedToken = await tokenManagement.verifyTokn(token);
   
   if (!verifiedToken) {
     return res.status(401).send("User is not authenticated");
