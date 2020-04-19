b055738c7674a9ae7deee97203d74bdbbda12726
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-20 00:44:04 +0300

    check if user is authenticated

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,26 +1,15 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/users.model");
-const passport = require("passport");
+const tokenManagement = require("../Utilis/tokenmanagement");
 
-require("dotenv").config({ path: "./config/.env" });
+const auth = async (req, res, next) => {
 
-const auth = async (req, res, next) => {  
-  try {
-    const token = req.header("Authorization").replace("JWT ", "");
-    const decoded = jwt.verify(token, process.env.JWT_PRIVATEKEY);
-    const user = await User.findOne({ _id: decoded.id });
-    
-    if (!user) {
-      throw new Error;
-    }
-
-    req.token = token;
-    req.user = user;
-
-    next();
-  } catch (error) {
-    res.status(401).send({ error: "User is not authenticate" });
+  const token = req.get("Authorization");
+  const verifiedToken = tokenManagement.verifyTokn(token);
+  
+  if (!verifiedToken) {
+    return res.status(401).send("User is not authenticated");
   }
+
+  next();
 };
 
 module.exports = auth;
