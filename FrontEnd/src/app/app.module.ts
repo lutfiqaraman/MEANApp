d88fc5628340162e32b35ca0d38223d9980a34d7
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-28 23:45:08 +0300

    create account component

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
-    NotFoundComponent
+    NotFoundComponent,
+    AccountsComponent
   ],
   imports: [
     BrowserModule,
