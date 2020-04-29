f6e4ad835a49348fb89e9a9a5b3a0b232cf59b4e
lutfiqaraman <lutfiqaraman@gmail.com>
2020-04-29 15:46:06 +0300

    add login and userauth function

--- FrontEnd/src/app/services/auth.service.ts
+++ FrontEnd/src/app/services/auth.service.ts
@@ -2,25 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   authToken: any;
   user: any;
   apiServer: string = environment.apiServer;
+  url: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   registerUser(user) {
     const headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
 
-    const url = this.apiServer +  '/users/register';
+    const url = this.apiServer + '/users/register';
     const header = { headers };
 
-    return this.http.post(url, user, header).pipe(map(res => res));
+    return this.http.post(url, user, header).pipe(map((res) => res));
   }
 
   authenticateUser(user) {
@@ -30,7 +32,29 @@ export class AuthService {
     const url = this.apiServer + '/users/auth';
     const header = { headers };
 
-    return this.http.post(url, user, header).pipe(map(res => res));
+    return this.http.post(url, user, header).pipe(map((res) => res));
+  }
+
+  login(name: string, password: string): Observable<any> {
+    this.url = this.apiServer + '/users/login';
+
+    const result = this.http.post<any>(this.url, {
+      name,
+      password,
+    });
+
+    return result;
+  }
+
+  authUser(): Observable<boolean> {
+    let token: string;
+    this.url = this.apiServer + '/users/auth';
+
+    if (typeof localStorage !== 'undefined') {
+      token = localStorage.token ? localStorage.token : '';
+    }
+
+    return this.http.post<boolean>(this.url, { token });
   }
 
   getProfile() {
